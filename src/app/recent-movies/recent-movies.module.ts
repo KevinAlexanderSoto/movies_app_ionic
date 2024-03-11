3ea245cfde5d393211c20c9fb9e4901fc7447547
kevin alexander soto <kevinalexandersoto999@gmail.com>
2024-03-10 21:31:04 -0500

    feature: add new ionic swiper-slide

--- src/app/recent-movies/recent-movies.module.ts
+++ src/app/recent-movies/recent-movies.module.ts
@@ -1,5 +1,5 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RecentMoviesPage } from './recent-movies.page';
@@ -15,6 +15,7 @@ import { RecentMoviesPageRoutingModule } from './recent-movies-routing.module';
     ExploreContainerComponentModule,
     RecentMoviesPageRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [RecentMoviesPage]
 })
 export class RecentMoviesPageModule { }
